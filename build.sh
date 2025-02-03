683ebba04965eb1bd63bc0e4234e5aa45b0527e2
Barrett Williamson <williamsonbarrett@gmail.com>
2025-02-03 14:58:36 -0500

    feat: add tailscale

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 
 set -ouex pipefail
 
+MAJOR_VERSION_NUMBER="$(sh -c '. /usr/lib/os-release ; echo $VERSION_ID | cut -d'.' -f1')"
+export MAJOR_VERSION_NUMBER
+
 dnf install -y tmux 
+
+dnf config-manager --add-repo "https://pkgs.tailscale.com/stable/rhel/${MAJOR_VERSION_NUMBER}/tailscale.repo"
+dnf config-manager --set-disabled tailscale-stable
+dnf -y --enablerepo tailscale-stable install \
+	tailscale
 
 systemctl enable podman.socket
